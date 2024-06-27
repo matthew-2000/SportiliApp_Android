fb0a7dd76b4607a9ecf8f173ffe48663029b0c53
matthew-2000 <m.ercolino1@studenti.unisa.it>
2024-06-27 17:34:41 +0200

    add icon and color scheme

--- app/src/main/java/com/matthew/sportiliapp/ui/theme/Theme.kt
+++ app/src/main/java/com/matthew/sportiliapp/ui/theme/Theme.kt
@@ -12,15 +12,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Orange80,
+    secondary = OrangeGrey80,
+    tertiary = OrangePink80
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Orange40,
+    secondary = OrangeGrey40,
+    tertiary = OrangePink40
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -41,10 +41,6 @@ fun SportiliAppTheme(
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
 
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
