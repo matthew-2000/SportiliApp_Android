61b15c410d39f61f3defe64b0e434230c372ecfd
matthew-2000 <m.ercolino1@studenti.unisa.it>
2025-11-03 12:25:22 +0100

    Bump version code to 24 and version name to 1.2.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.matthew.sportiliapp"
         minSdk = 27
         targetSdk = 35
-        versionCode = 23
-        versionName = "1.2.5"
+        versionCode = 24
+        versionName = "1.2.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
